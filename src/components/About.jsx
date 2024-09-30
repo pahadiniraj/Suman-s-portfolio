a154f3f5954e0fc003d5f9d45d6a2834385ceea6
pahadiniraj <sharma12345niraj@gmail.com>
2024-09-30 22:03:18 +0545

    animation on progess

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import {
-  boxShadowOptional,
-  boxShadowPrimary,
-  boxShadowSecondary,
   buttonPrimary,
-  buttonSecondary,
   headingPrimary,
   titlePrimary,
 } from "../utils/StyleUtils";
@@ -12,20 +8,49 @@ import avatar from "../assets/images/avatar.jpg";
 import { serviceList } from "../assets/dummyData/dummyData";
 import ScrollTrigger from "react-scroll-trigger";
 import { motion } from "framer-motion";
-import { textVariants } from "../utils/Framer";
 
 export default function About() {
   const [onEnter, setOnEnter] = useState(false);
+  const [onEnterDo, setOnEnterDo] = useState(false);
 
-  const handleEnterViewport = () => {
-    setOnEnter(true);
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5, // Delay between children animations
+      },
+    },
   };
+
+  const childVariants = {
+    hidden: { opacity: 0, x: 100 }, 
+    visible: { opacity: 1, x: 0 }, 
+  };
+
+  const handleScrollTrigger1 = ({ progress }) => {
+    if (progress >= 0.2) {
+      setOnEnter(true); // Trigger animation when 90% is visible
+    }
+  };
+
+  const handleScrollTrigger2 = ({ progress }) => {
+    if (progress >= 0.2) {
+      setOnEnterDo(true); // Trigger animation when 90% is visible
+    }
+  };
+
   return (
     <div
-      className={`px-3 py-10 md:p-20 bg-gray-500 bg-opacity-10 backdrop-blur-sm  space-y-10`}
+      className={`px-3 py-10 md:p-20 bg-gray-500 bg-opacity-10 backdrop-blur-sm overflow-x-auto  space-y-10`}
     >
-      <ScrollTrigger onEnter={handleEnterViewport}>
-        <div className="border-l-4 border-primary pl-2">
+      <ScrollTrigger onProgress={handleScrollTrigger1}>
+        <motion.div
+          className="border-l-4 border-primary pl-2"
+          initial={{ opacity: 0, y: 50 }}
+          animate={onEnter && { opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <motion.p
             initial={{ opacity: 0, y: 50 }}
             animate={onEnter && { opacity: 1, y: 0 }}
@@ -41,7 +66,7 @@ export default function About() {
           >
             ABOUT ME
           </motion.p>
-        </div>
+        </motion.div>
         <div className="flex flex-col md:flex-row gap-5">
           <motion.div
             initial={{ opacity: 0, x: -100 }}
@@ -101,23 +126,40 @@ export default function About() {
           </motion.div>
         </div>
       </ScrollTrigger>
-
-      <div className="space-y-5">
-        <p className={`text-center ${headingPrimary}`}>WHAT I DO FOR YOU?</p>
-        <hr />
-        <div className="flex flex-col md:flex-row gap-5">
-          {serviceList.map((service, index) => (
-            <div
-              key={index}
-              className="shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] backdrop-blur-3xl p-5 rounded-xl flex flex-col items-center justify-center text-center gap-5"
-            >
-              <p className="text-2xl font-bold text-primary">{service.title}</p>
-              <span className={`${service.imageUrl} text-6xl`}></span>
-              <p>{service.description}</p>
-            </div>
-          ))}
+      <ScrollTrigger onProgress={handleScrollTrigger2}>
+        <div className="space-y-5 ">
+          <motion.p
+            className={`text-center ${headingPrimary}`}
+            initial={{ opacity: 0, x: 100 }}
+            animate={onEnterDo && { opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            WHAT I DO FOR YOU?
+          </motion.p>
+          <hr />
+          <motion.div
+            className="flex flex-col md:flex-row gap-5 overflow-hidden "
+            variants={containerVariants}
+            initial="hidden"
+            animate={onEnterDo && "visible"} // Control animation on scroll enter
+          >
+            {serviceList.map((service, index) => (
+              <motion.div
+                key={index}
+                className="shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] backdrop-blur-3xl p-5 rounded-xl flex flex-col items-center justify-center text-center gap-5  overflow-hidden "
+                variants={childVariants}
+                transition={{ duration: 0.5 }}
+              >
+                <p className="text-2xl font-bold text-primary">
+                  {service.title}
+                </p>
+                <span className={`${service.imageUrl} text-6xl`}></span>
+                <p>{service.description}</p>
+              </motion.div>
+            ))}
+          </motion.div>
         </div>
-      </div>
+      </ScrollTrigger>
     </div>
   );
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,109 +1,199 @@
-import React from "react";
+import React, { useState } from "react";
 import { titlePrimary } from "../utils/StyleUtils";
+import { motion } from "framer-motion";
+import ScrollTrigger from "react-scroll-trigger";
 
 export default function Resume() {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5, // Delay between children animations
+      },
+    },
+  };
+
+  const childVariants1 = {
+    hidden: { opacity: 0, x: -100 }, // Start off-screen to the right
+    visible: { opacity: 1, x: 0 }, // Animate to on-screen
+  };
+
+  const childVariants2 = {
+    hidden: { opacity: 0, x: 100 }, // Start off-screen to the right
+    visible: { opacity: 1, x: 0 }, // Animate to on-screen
+  };
+
+  const [onEnter, setOnEnter] = useState(false);
+  const [onEnterDo, setOnEnterDo] = useState(false);
+
+  const handleScrollTrigger1 = ({ progress }) => {
+    if (progress >= 0.5) {
+      setOnEnter(true); // Trigger animation when 90% is visible
+    }
+  };
+  const handleScrollTrigger2 = ({ progress }) => {
+    if (progress >= 0.2) {
+      setOnEnterDo(true); // Trigger animation when 90% is visible
+    }
+  };
+
   return (
     <div className="px-3 py-10 md:p-20 space-y-10">
-      <div className="border-l-4 border-primary pl-2">
-        <p className="">MY EDUCTION AND EXPERIENCES</p>
-        <p className={titlePrimary}>RESUME</p>
-      </div>
+      <ScrollTrigger onProgress={handleScrollTrigger1}>
+        <motion.div
+          className="border-l-4 border-primary pl-2"
+          initial={{ opacity: 0, y: 50 }}
+          animate={onEnter && { opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <p className="">MY EDUCATION AND EXPERIENCES</p>
+          <p className={titlePrimary}>RESUME</p>
+        </motion.div>
+      </ScrollTrigger>
+      <ScrollTrigger onProgress={handleScrollTrigger2}>
+        <div className="flex flex-col md:flex-row gap-5">
+          <motion.div
+            variants={containerVariants}
+            initial="hidden"
+            animate={onEnterDo && "visible"}
+            className="w-full rounded-xl overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] space-y-2 pb-2"
+          >
+            <motion.p
+              className="bg-custom-primary p-3 text-2xl font-bold text-center text-black"
+              initial={{ opacity: 0, x: -1000 }}
+              animate={onEnterDo && { opacity: 1, x: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              Education
+            </motion.p>
 
-      <div className="flex flex-col md:flex-row gap-5">
-        <div className="w-full rounded-xl overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] space-y-2 pb-2">
-          <p className="bg-custom-primary p-3 text-2xl font-bold text-center text-black ">
-            Education
-          </p>
+            <motion.div
+              variants={childVariants1}
+              transition={{ duration: 0.5 }}
+              className="flex items-center gap-5 p-5  bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+            >
+              <div>
+                <i className="fa-solid fa-graduation-cap w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
+              </div>
+              <div className="">
+                <p className="font-bold">2018-2023</p>
+                <p className="text-xl font-bold text-primary">
+                  Bachelor's Degree in Lorem ipsum
+                </p>
+                <p className="text-lg font-bold">Lorem University</p>
+                <p className="font-medium">Lorem City, USA</p>
+              </div>
+            </motion.div>
 
-          <div className="flex items-center gap-5 p-5 mx-2 rounded-lg bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <div>
-              <i className="fa-solid fa-graduation-cap w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
-            </div>
-            <div className="">
-              <p className="font-bold">2018-2023</p>
-              <p className="text-xl font-bold text-primary">
-                Bachelor's Degree in Lorem ipsum dolor sit amet
-              </p>
-              <p className="text-lg font-bold">Lorem University</p>
-              <p className="font-medium">Lorem City, USA</p>
-            </div>
-          </div>
+            <motion.div
+              variants={childVariants1}
+              transition={{ duration: 0.5 }}
+              className="flex items-center gap-5 p-5  bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+            >
+              <div>
+                <i className="fa-solid fa-graduation-cap w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
+              </div>
+              <div className="">
+                <p className="font-bold">2020-2022</p>
+                <p className="text-xl font-bold text-primary">
+                  Master's Degree in Lorem
+                </p>
+                <p className="text-lg font-bold">Lorem University</p>
+                <p className="font-medium">Lorem City, USA</p>
+              </div>
+            </motion.div>
 
-          <div className="flex items-center gap-5 p-5 mx-2 rounded-lg bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <div>
-              <i className="fa-solid fa-graduation-cap w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
-            </div>
-            <div className="">
-              <p className="font-bold">2020-2022</p>
-              <p className="text-xl font-bold text-primary">
-                Master's Degree in Lorem ipsum dolor sit amet
-              </p>
-              <p className="text-lg font-bold">Lorem University</p>
-              <p className="font-medium">Lorem City, USA</p>
-            </div>
-          </div>
+            <motion.div
+              variants={childVariants1}
+              transition={{ duration: 0.5 }}
+              className="flex items-center gap-5 p-5  bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+            >
+              <div>
+                <i className="fa-solid fa-graduation-cap w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
+              </div>
+              <div className="">
+                <p className="font-bold">2014-2019</p>
+                <p className="text-xl font-bold text-primary">
+                  Bachelor's Degree in Lorem ipsum dolor
+                </p>
+                <p className="text-lg font-bold">Lorem University</p>
+                <p className="font-medium">Lorem City, USA</p>
+              </div>
+            </motion.div>
+          </motion.div>
 
-          <div className="flex items-center gap-5 p-5 mx-2 rounded-lg bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <div>
-              <i className="fa-solid fa-graduation-cap w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
-            </div>
-            <div className="">
-              <p className="font-bold">2014-2019</p>
-              <p className="text-xl font-bold text-primary">
-                Bachelor's Degree in Lorem ipsum dolor sit amet
-              </p>
-              <p className="text-lg font-bold">Lorem University</p>
-              <p className="font-medium">Lorem City, USA</p>
-            </div>
-          </div>
+          <motion.div
+            variants={containerVariants}
+            initial="hidden"
+            animate={onEnterDo && "visible"}
+            className="w-full rounded-xl overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] space-y-2 pb-2"
+          >
+            <motion.p
+              initial={{ opacity: 0, x: 1000 }}
+              animate={onEnterDo && { opacity: 1, x: 0 }}
+              transition={{ duration: 0.4 }}
+              className="bg-custom-primary p-3 text-2xl font-bold text-center text-black"
+            >
+              Experiences
+            </motion.p>
+
+            <motion.div
+              variants={childVariants2}
+              transition={{ duration: 0.5 }}
+              className="flex items-center gap-5 p-5  bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+            >
+              <div>
+                <i className="fa-solid fa-briefcase w-10 h-10 rounded-full bg-custom-primary flex items-center justify-center text-black"></i>
+              </div>
+              <div className="">
+                <p className="font-bold">2018-2023</p>
+                <p className="text-xl font-bold text-primary">
+                  Senior Lorem ipsum
+                </p>
+                <p className="text-lg font-bold">Lorem Inc.</p>
+                <p className="font-medium">Lorem City, USA</p>
+              </div>
+            </motion.div>
+
+            <motion.div
+              variants={childVariants2}
+              transition={{ duration: 0.5 }}
+              className="flex items-center gap-5 p-5  bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+            >
+              <div>
+                <i className="fa-solid fa-briefcase w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
+              </div>
+              <div className="">
+                <p className="font-bold">2020-2022</p>
+                <p className="text-xl font-bold text-primary">
+                  Junior Lorem ipsum
+                </p>
+                <p className="text-lg font-bold">Lorem Inc.</p>
+                <p className="font-medium">Lorem City, USA</p>
+              </div>
+            </motion.div>
+
+            <motion.div
+              variants={childVariants2}
+              transition={{ duration: 0.5 }}
+              className="flex items-center gap-5 p-5  bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+            >
+              <div>
+                <i className="fa-solid fa-briefcase w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
+              </div>
+              <div className="">
+                <p className="font-bold">2014-2019</p>
+                <p className="text-xl font-bold text-primary">
+                  Intern Lorem ipsum
+                </p>
+                <p className="text-lg font-bold">Lorem Inc.</p>
+                <p className="font-medium">Lorem City, USA</p>
+              </div>
+            </motion.div>
+          </motion.div>
         </div>
-
-        <div className="w-full rounded-xl overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] space-y-2 pb-2">
-          <p className="bg-custom-primary p-3 text-2xl font-bold text-center text-black">
-            Experiences
-          </p>
-
-          <div className="flex items-center gap-5 p-5 mx-2 rounded-lg bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <div>
-              <i className="fa-solid fa-briefcase w-10 h-10 rounded-full bg-custom-primary flex items-center justify-center text-black"></i>
-            </div>
-            <div className="">
-              <p className="font-bold">2018-2023</p>
-              <p className="text-xl font-bold text-primary">
-                Senior Lorem ipsum
-              </p>
-              <p className="text-lg font-bold">Lorem Inc.</p>
-              <p className="font-medium">Lorem City, USA</p>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-5 p-5 mx-2 rounded-lg bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <div>
-              <i className="fa-solid fa-briefcase w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
-            </div>
-            <div className="">
-              <p className="font-bold">2020-2022</p>
-              <p className="text-xl font-bold text-primary">
-                Junior Lorem ipsum
-              </p>
-              <p className="text-lg font-bold">Lorem Inc.</p>
-              <p className="font-medium">Lorem City, USA</p>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-5 p-5 mx-2 rounded-lg bg-gray-300 bg-opacity-10 backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <div>
-              <i className="fa-solid fa-briefcase w-10 h-10 rounded-full bg-primary flex items-center justify-center text-black"></i>
-            </div>
-            <div className="">
-              <p className="font-bold">2014-2019</p>
-              <p className="text-xl font-bold text-primary">Lorem ipsum</p>
-              <p className="text-lg font-bold">Lorem Inc.</p>
-              <p className="font-medium">Lorem City, USA</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      </ScrollTrigger>
     </div>
   );
 }
